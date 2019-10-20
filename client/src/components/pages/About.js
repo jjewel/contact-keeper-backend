d0aefc5463e53b3305bc0796726ce31e2086889c
Jason Jewel <jjewe2009@gmail.com>
2019-10-21 00:11:25 +1000

    Added About Page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>This is a full stack MERN application</p>
+      <a
+        className='btn btn-dark'
+        href='https://github.com/jjewel/contact-keeper'
+      >
+        <i class='fab fa-github'></i>&nbsp; Github Source
+      </a>
+    </div>
+  );
 };
 
 export default About;
