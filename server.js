818ebe79e1a02051c5803a2f28150c510fd0ddc1
Jason Jewel <jjewe2009@gmail.com>
2019-10-20 18:38:40 +1000

    Added route files

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API' })
+);
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
