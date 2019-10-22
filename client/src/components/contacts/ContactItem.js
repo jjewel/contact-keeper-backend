b701e82665091cee8739e704276c183ff18f61dc
Jason Jewel <jjewe2009@gmail.com>
2019-10-22 12:28:52 +1000

    Add delete contact action

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -3,8 +3,13 @@ import ContactContext from '../../context/contact/contactContext';
 import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact }) => {
+  const { deleteContact } = useContext(ContactContext);
+
   const { id, name, email, phone, type } = contact;
-  const contactContext = useContext(ContactContext);
+
+  const onDelete = () => {
+    deleteContact(id);
+  };
 
   return (
     <div className='card bg-light'>
@@ -34,7 +39,9 @@ const ContactItem = ({ contact }) => {
       </ul>
       <p>
         <button className='btn btn-dark btn-sm'>Edit</button>
-        <button className='btn btn-danger btn-sm'>Delete</button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
