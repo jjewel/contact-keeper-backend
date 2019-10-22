ab14c36d6cb2796e37252d6556bea740284fc0c0
Jason Jewel <jjewe2009@gmail.com>
2019-10-22 13:49:44 +1000

    Add filter contact component

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -3,13 +3,20 @@ import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
-  const { contacts } = useContext(ContactContext);
+  const { contacts, filtered } = useContext(ContactContext);
 
+  if (contacts.length === 0) {
+    return <h4>No Contacts...</h4>;
+  }
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered
+        ? filtered.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 };
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -30,7 +30,8 @@ const ContactState = props => {
         type: 'professional'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -54,7 +55,7 @@ const ContactState = props => {
   };
 
   // Clear Current Contact
-  const clearCurrent = contact => {
+  const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
 
@@ -64,19 +65,28 @@ const ContactState = props => {
   };
 
   // Filter Contacts
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   // Clear Filter
+  const clearFilter = contact => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
