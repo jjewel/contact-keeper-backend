4108b7f7c62f2bca340acc694789f884049aaba1
Jason Jewel <jjewe2009@gmail.com>
2019-10-22 14:46:20 +1000

    Mocked out Auth Context

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,27 @@
+import React, { useReducer } from 'react';
+import AuthContext from './authContext';
+import AuthReducer from './authReducer';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+const AuthState = props => {
+  const initialState = {};
+
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  // Actions
+
+  return (
+    <AuthContext.Provider value={{}}>{props.children}</AuthContext.Provider>
+  );
+};
+
+export default AuthState;
